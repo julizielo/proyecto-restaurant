bc3dc1a5c532ac18a2ecb1ace85f976dc185e564
juanoxidev <jm.oxinalde@hotmail.com>
2023-04-24 22:29:29 -0300

    form validado

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 let enviar = document.getElementById("enviar");
-
 enviar.addEventListener("click", () => {
-  alert("Suscripcion Exitosa");
+  validarFormulario();
 });
+
+function validarFormulario() {
+  // Obtener los valores de los campos
+  let nombre = document.getElementById("nombreCompleto").value;
+  let telefono = document.getElementById("tel").value;
+  let correo = document.getElementById("email").value;
+
+  // Verificar que los campos no estén vacíos
+  if (nombre == "" || telefono == "" || correo == "") {
+    alert("Por favor, complete todos los campos.");
+  } else {
+    alert("Suscripcion exitosa");
+  }
+}
